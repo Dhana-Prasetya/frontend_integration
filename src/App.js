b3f5ac061c3974205cc046f12f574714f22bd093
Dhana <dhana.pns@gmail.com>
2025-11-05 21:07:04 +0700

    in progress

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import PrivateRoute from './components/PrivateRoute';
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<h3 className="text-center mt-5">404 Not Found</h3>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        
+    <div className='mx-auto text-center' style={{ maxWidth: '600px' }}>
+        <h1 className='fs-4 fw-bold'>404 Not Found</h1>
+        <h2 className='fs-6 text-secondary'>The page you are looking for does not exist.</h2>
+    </div>
+    )
+}
+
+export default NotFound
